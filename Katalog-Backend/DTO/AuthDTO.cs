03dd4969aec789bc2b8eea9f7a4508371722b20d
ruran8wa <princerurangwa01@gmail.com>
2026-08-26 15:51:36 +0200

    feat: added register, login and authResponse DTOs + AuthService interface

---
+++ Katalog-Backend/DTO/AuthDTO.cs
@@ -0,0 +1,6 @@
+namespace Katalog_Backend.DTO;
+
+public class AuthDTO
+{
+    
+}
---
+++ Katalog-Backend/DTOs/AuthDTO.cs
@@ -0,0 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Katalog_Backend.DTO;
+
+public class RegisterDto
+{
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+    
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Required]
+    [MinLength(8)]
+    public string Password { get; set; }
+}
+
+public class LoginDto
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Required]
+    [MinLength(8)]
+    public string Password { get; set; }
+}
+
+public record AuthResponseDto
+(
+     string Token,
+     string Email, 
+     string Role 
+);
