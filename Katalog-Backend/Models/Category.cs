78b6b2409d5a6709335711aafac02058c62f9612
ruran8wa <princerurangwa01@gmail.com>
2026-08-31 18:37:40 +0200

    refactor: adding value and required modifier to non-nullable properties

--- Katalog-Backend/Models/Category.cs
+++ Katalog-Backend/Models/Category.cs
@@ -3,10 +3,10 @@ namespace Katalog_Backend.Models;
 public class Category
 {
     public  int Id { get; set; }
-    public string CategoryName {get; set;}
+    public string CategoryName {get; set;} =  string.Empty;
     public int? CategoryParentId { get; set; }
     
-    public Category Parent { get; set; }
+    public Category? Parent { get; set; }
     public ICollection<Category> Children { get; set; }
     public ICollection<Product> Products { get; set; }
 }
